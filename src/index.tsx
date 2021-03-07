f8894636fe9711082c011f6653c4dcff6aa3622b
Einlanzerous <einlanzerous@live.com>
2021-03-07 15:54:42 -0500

    Add event listener to show code in a sandboxed iFrame

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ const ESBUILD_WASM_VER = 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm';
 
 const App = () => {
   const ref = useRef<esbuild.Service>();
+  const iframe = useRef<HTMLIFrameElement>(null);
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -27,6 +27,10 @@ const App = () => {
       return;
     }
 
+    if (iframe !== null && iframe.current !== null) {
+      iframe.current.srcdoc = html;
+    }
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -38,8 +42,28 @@ const App = () => {
       }
     });
 
-    setCode(result.outputFiles[0].text);
+    iframe?.current?.contentWindow?.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id='root'></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -50,7 +74,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        title="Code iFrame Preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
